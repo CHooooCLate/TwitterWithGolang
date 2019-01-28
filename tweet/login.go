b6d4dd62bfe5c39c7f959fa784a4f1640c577ad3
CHooooCLate <gaogaogao_masaya@rrcs-172-254-99-52.nyc.biz.rr.com>
2019-01-29 00:49:44 +0900

    use DynamoDB

--- tweet/login.go
+++ tweet/login.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+    "fmt"
+
     "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
     "github.com/gomodule/oauth1/oauth"
     "github.com/joho/godotenv"
 
     "log"
     "os"
+    "time"
 
     // デバッグ用
     // spew.Dump(value)
-    //"github.com/davecgh/go-spew/spew"
+    "github.com/davecgh/go-spew/spew"
     // reflect.TypeOf(value)
     //"reflect"
 )
+
+type Token struct {
+  Id int `dynamo:"id"`
+  OauthToken string `dynamo:"oauth_token"`
+  SecretToken string `dynamo:"secret_token"`
+  RegisterDate string `dynamo:"register_date"`
+}
 
 func loadEnv() {
     err := godotenv.Load("/var/task/tweet/.env")
@@ -43,6 +56,44 @@ func Handler() (string, error) {
     if err != nil {
         log.Fatal("RequestTemporaryCredentials:", err)
     }
+
+    spew.Dump(tempCredentials)
+
+    // session
+    sess, err := session.NewSession()
+    if err != nil {
+        panic(err)
+    }
+
+    svc := dynamodb.New(sess)
+
+    // 時間取得時のフォーマット指定
+    format := "2006-01-02 15:04:05"
+
+    // PutItem
+    putParams := &dynamodb.PutItemInput{
+        TableName: aws.String("Token"),
+        Item: map[string]*dynamodb.AttributeValue{
+            "id": {
+                N: aws.String("0"),
+            },
+            "oauth_token": {
+                S: aws.String(tempCredentials.Token),
+            },
+            "secret_token": {
+                S: aws.String(tempCredentials.Secret),
+            },
+            "register_date": {
+                S: aws.String(time.Now().Format(format)),
+            },
+        },
+    }
+
+    putItem, putErr := svc.PutItem(putParams)
+    if putErr != nil {
+        panic(putErr)
+    }
+    fmt.Println(putItem)
 
     authorizeUrl := oauthClient.AuthorizationURL(tempCredentials, nil)
 
